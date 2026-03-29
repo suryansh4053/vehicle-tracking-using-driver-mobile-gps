0fabfea702e4f689ab19af4f6e0df77035a8fe48
suryansh4053 <itzmaxton4000@gmail.com>
2026-03-29 22:30:19 +0530

    Update build.gradle.kts

--- AndroidStudioProjects/Vehicle_tracking_using_driver_mobile_gps_tracking/build.gradle.kts
+++ AndroidStudioProjects/Vehicle_tracking_using_driver_mobile_gps_tracking/build.gradle.kts
@@ -1,4 +1,56 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.vehicle_tracking_using_driver_mobile_gps_tracking"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.vehicle_tracking_using_driver_mobile_gps_tracking"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
